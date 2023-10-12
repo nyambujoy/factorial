0debca81ba6649bb5e564885a2f057e9e9728739
nyambujoy <131836946+nyambujoy@users.noreply.github.com>
2023-10-12 15:52:19 +0300

    Create fact.js

---
+++ fact.js
@@ -0,0 +1,27 @@
+let n =4
+function factorial(n)
+{
+let answer = 1;
+if (answer === 0 || answer) < 1{
+return answer;
+}else if (answer>1){
+for(let i =1;i >=1; --I){
+answer = answer * i
+return answer;
+}else{
+return "number has to be positive"}
+}
+
+
+//alternatively you can use recursion
+
+function factorial(n){
+if (n < 1){
+return "numer must be positive";
+
+if (answer === 0 || answer) < 1{
+return 1;
+}else{
+ n(factorial(n-1)
+
+
